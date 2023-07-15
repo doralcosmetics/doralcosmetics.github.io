6fd868f62d56e055d80d2780371c37dfd7eca1f0
doralcosmetics <doralcosmetics@163.com>
2023-07-15 15:23:57 +0800

    1. 修改为英文 2. banner只展示

--- js/data.js
+++ js/data.js
@@ -1,56 +1,79 @@
-let data ={
-    productList:[
-        {'id': 1, 'title': '沐浴露', 'price': 12.1, 'img': ["https://unpkg.com/outeres/demo/carousel/720x360-1.jpg","https://unpkg.com/outeres/demo/carousel/720x360-2.jpg","https://unpkg.com/outeres@0.0.11/demo/wallpaper.jpg"],'detail':'jQuery是一种非常流行和强大的JavaScript库。它提供了很多方便的方法来操作HTML元素和文档对象模型（DOM）。其中一项非常有用的功能是用jquery设置元素的文本。\n' +
-                '\n' +
-                '在本文中，我们将探讨如何使用jquery来设置元素的文本。我们将介绍两种方法：.text()和.html()。\n' +
-                '\n' +
-                '方法一：.text()\n' +
-                '\n' +
-                '.text()方法是jqury中用来设置元素文本的最基本方法。\n' +
-                '\n' +
-                '要使用.text()方法，您可以首先选取要设置文本的元素。例如，假设您有以下HTML代码：'},
-        {'id': 2, 'title': '沐浴露1', 'price': 12.1, 'img': ["https://unpkg.com/outeres/demo/carousel/720x360-2.jpg"],'detail':'jQuery是一种非常流行和强大的JavaScript库。它提供了很多方便的方法来操作HTML元素和文档对象模型（DOM）。其中一项非常有用的功能是用jquery设置元素的文本。\n' +
-                '\n' +
-                '在本文中，我们将探讨如何使用jquery来设置元素的文本。我们将介绍两种方法：.text()和.html()。\n' +
-                '\n' +
-                '方法一：.text()\n' +
-                '\n' +
-                '.text()方法是jqury中用来设置元素文本的最基本方法。\n' +
-                '\n' +
-                '要使用.text()方法，您可以首先选取要设置文本的元素。例如，假设您有以下HTML代码：'},
-        {'id': 3, 'title': '沐浴露2', 'price': 12.1, 'img': ["https://unpkg.com/outeres/demo/carousel/720x360-3.jpg"],'detail':'jQuery是一种非常流行和强大的JavaScript库。它提供了很多方便的方法来操作HTML元素和文档对象模型（DOM）。其中一项非常有用的功能是用jquery设置元素的文本。\n' +
-                '\n' +
-                '在本文中，我们将探讨如何使用jquery来设置元素的文本。我们将介绍两种方法：.text()和.html()。\n' +
-                '\n' +
-                '方法一：.text()\n' +
-                '\n' +
-                '.text()方法是jqury中用来设置元素文本的最基本方法。\n' +
-                '\n' +
-                '要使用.text()方法，您可以首先选取要设置文本的元素。例如，假设您有以下HTML代码：'},
-        {'id': 4, 'title': '沐浴露3', 'price': 12.1, 'img': ["https://unpkg.com/outeres/demo/carousel/720x360-4.jpg"],'detail':'jQuery是一种非常流行和强大的JavaScript库。它提供了很多方便的方法来操作HTML元素和文档对象模型（DOM）。其中一项非常有用的功能是用jquery设置元素的文本。\n' +
-                '\n' +
-                '在本文中，我们将探讨如何使用jquery来设置元素的文本。我们将介绍两种方法：.text()和.html()。\n' +
-                '\n' +
-                '方法一：.text()\n' +
-                '\n' +
-                '.text()方法是jqury中用来设置元素文本的最基本方法。\n' +
-                '\n' +
-                '要使用.text()方法，您可以首先选取要设置文本的元素。例如，假设您有以下HTML代码：'},
-        {'id': 5, 'title': '沐浴露4', 'price': 12.1, 'img': ["https://unpkg.com/outeres/demo/carousel/720x360-5.jpg"],'detail':'jQuery是一种非常流行和强大的JavaScript库。它提供了很多方便的方法来操作HTML元素和文档对象模型（DOM）。其中一项非常有用的功能是用jquery设置元素的文本。\n' +
-                '\n' +
-                '在本文中，我们将探讨如何使用jquery来设置元素的文本。我们将介绍两种方法：.text()和.html()。\n' +
-                '\n' +
-                '方法一：.text()\n' +
-                '\n' +
-                '.text()方法是jqury中用来设置元素文本的最基本方法。\n' +
-                '\n' +
-                '要使用.text()方法，您可以首先选取要设置文本的元素。例如，假设您有以下HTML代码：'},
+let data = {
+        head:[
+                "https://unpkg.com/outeres/demo/carousel/720x360-1.jpg","./image/jessica.png"
 
-    ],
-    contact:{
-        phone: '123456789',
-        email: '1234566789@qq.com',
-        address: '中国'
-    }
+        ],
+        productList: [
+                {
+                        'id': 1, 'title': 'Lace wig glue', 'price': '$3/pc', 'img': ["https://unpkg.com/outeres/demo/carousel/720x360-1.jpg", "https://unpkg.com/outeres/demo/carousel/720x360-2.jpg", "https://unpkg.com/outeres@0.0.11/demo/wallpaper.jpg"], 'detail': 'jQuery是一种非常流行和强大的JavaScript库。它提供了很多方便的方法来操作HTML元素和文档对象模型（DOM）。其中一项非常有用的功能是用jquery设置元素的文本。\n' +
+                                '\n' +
+                                '在本文中，我们将探讨如何使用jquery来设置元素的文本。我们将介绍两种方法：.text()和.html()。\n' +
+                                '\n' +
+                                '方法一：.text()\n' +
+                                '\n' +
+                                '.text()方法是jqury中用来设置元素文本的最基本方法。\n' +
+                                '\n' +
+                                '要使用.text()方法，您可以首先选取要设置文本的元素。例如，假设您有以下HTML代码：'
+                },
+                {
+                        'id': 2, 'title': 'Lace tint spray', 'price': '$3.5/pc', 'img': ["https://unpkg.com/outeres/demo/carousel/720x360-2.jpg"], 'detail': 'jQuery是一种非常流行和强大的JavaScript库。它提供了很多方便的方法来操作HTML元素和文档对象模型（DOM）。其中一项非常有用的功能是用jquery设置元素的文本。\n' +
+                                '\n' +
+                                '在本文中，我们将探讨如何使用jquery来设置元素的文本。我们将介绍两种方法：.text()和.html()。\n' +
+                                '\n' +
+                                '方法一：.text()\n' +
+                                '\n' +
+                                '.text()方法是jqury中用来设置元素文本的最基本方法。\n' +
+                                '\n' +
+                                '要使用.text()方法，您可以首先选取要设置文本的元素。例如，假设您有以下HTML代码：'
+                },
+                {
+                        'id': 3, 'title': 'Lace glue remover', 'price':'$2.5/pc', 'img': ["https://unpkg.com/outeres/demo/carousel/720x360-3.jpg"], 'detail': 'jQuery是一种非常流行和强大的JavaScript库。它提供了很多方便的方法来操作HTML元素和文档对象模型（DOM）。其中一项非常有用的功能是用jquery设置元素的文本。\n' +
+                                '在本文中，我们将探讨如何使用jquery来设置元素的文本。我们将介绍两种方法：.text()和.html()。\n' +
+                                '\n' +
+                                '方法一：.text()\n' +
+                                '\n' +
+                                '.text()方法是jqury中用来设置元素文本的最基本方法。\n' +
+                                '\n' +
+                                '要使用.text()方法，您可以首先选取要设置文本的元素。例如，假设您有以下HTML代码：'
+                },
+                {
+                        'id': 4, 'title': 'Lace gel', 'price': '$3.5/pc', 'img': ["https://unpkg.com/outeres/demo/carousel/720x360-4.jpg"], 'detail': 'jQuery是一种非常流行和强大的JavaScript库。它提供了很多方便的方法来操作HTML元素和文档对象模型（DOM）。其中一项非常有用的功能是用jquery设置元素的文本。\n' +
+                                '\n' +
+                                '在本文中，我们将探讨如何使用jquery来设置元素的文本。我们将介绍两种方法：.text()和.html()。\n' +
+                                '\n' +
+                                '方法一：.text()\n' +
+                                '\n' +
+                                '.text()方法是jqury中用来设置元素文本的最基本方法。\n' +
+                                '\n' +
+                                '要使用.text()方法，您可以首先选取要设置文本的元素。例如，假设您有以下HTML代码：'
+                },
+                {
+                        'id': 5, 'title': 'Lace glue pen', 'price':'$2.5/pc', 'img': ["https://unpkg.com/outeres/demo/carousel/720x360-5.jpg"], 'detail': 'jQuery是一种非常流行和强大的JavaScript库。它提供了很多方便的方法来操作HTML元素和文档对象模型（DOM）。其中一项非常有用的功能是用jquery设置元素的文本。\n' +
+                                '\n' +
+                                '在本文中，我们将探讨如何使用jquery来设置元素的文本。我们将介绍两种方法：.text()和.html()。\n' +
+                                '\n' +
+                                '方法一：.text()\n' +
+                                '\n' +
+                                '.text()方法是jqury中用来设置元素文本的最基本方法。\n' +
+                                '\n' +
+                                '要使用.text()方法，您可以首先选取要设置文本的元素。例如，假设您有以下HTML代码：'
+                },
+                {
+                        'id': 6, 'title': 'Lace tint mousse ', 'price':'$3.5/pc', 'img': ["https://unpkg.com/outeres/demo/carousel/720x360-5.jpg", "./image/jessica.png"],
+                        'detail': '这是新一个商品'
+                }
+                // 新建商品,id 就增加+1,tittle就是商品名称，price就是价格，img就是商品的图片，可以把图片放到image文件中，图片名称不要有中文，可写多个图片
+                ,
+                {
+                        'id': 7, 'title': 'Lace tint mousse demo', 'price':'$3.5/pc', 'img': ["./image/jessica.png","./image/jessica.png","./image/jessica.png"],
+                        'detail': '这是新一个商品'
+                }
+
+        ],
+        contact: {
+                Whatsapp: '+86 13178844805',
+                Email: 'Doralcosmetics@163.com',
+                Address: 'China'
+        }
 
 }
